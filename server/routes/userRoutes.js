c93761da15841be880c5f60765ffa90676abd335
komal <komalharbola0207@gmail.com>
2026-06-11 13:20:50 +0530

    minor changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,5 +10,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserData);
-userRouter.get("/cars", protect, getCars);
+userRouter.get("/cars", getCars);
+// userRouter.get("/cars", protect, getCars);
 export default userRouter;
